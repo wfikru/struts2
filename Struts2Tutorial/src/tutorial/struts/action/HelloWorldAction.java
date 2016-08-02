39f54e748d7cfdcb4849563e2c12c0de0e925e0b
Fikru <Fikru@webe>
2016-08-01 22:43:29 -0400

    git refresher

---
+++ Struts2Tutorial/src/tutorial/struts/action/HelloWorldAction.java
@@ -0,0 +1,17 @@
+package tutorial.struts.action;
+
+public class HelloWorldAction {
+	private String name;
+
+	public String execute() throws Exception {
+		return "success";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
